a96236845a228523ef67f0a57782e1e36af97cc6
jtignino <jonathan.jat@hotmail.com>
2022-10-11 20:02:07 -0300

    Creacion de array, clase constructora, funciones con metodos push y reduce, funcion para el calculo del interes total generado.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
 let usuario = "admin";
 let contrasenia = "admin123";
 let saldo = 100000;
-let tasaPlazoFijo = 75.0;
-let tasaMercadopago = 54.0;
-let tasaCaucion = 50.0;
+let tasaPlazoFijo = 75.1;
+let tasaMercadopago = 54.2;
+let tasaCaucion = 50.3;
+
+// Creo un array vacío:
+const historial = [];
+
+// Creo la clase constructora de objetos que luego se guardarán en el historial:
+class Inversion {
+    constructor(tipoInversion, interesGanado, plazo, tna, fecha, capitalFinal) {
+        this.tipoInversion = tipoInversion;
+        this.interesGanado = parseFloat(interesGanado);
+        this.plazo = parseInt(plazo);
+        this.tna = parseInt(tna);
+        this.fecha = fecha;
+        this.capitalFinal = parseFloat(capitalFinal);
+    }
+}
+// fin de la clase constructora
+
+// Función para actualizar un array genérico:
+function cargarArray(arr, valor) {
+    arr.push(valor);
+}
+// fin de la función para actualizar el historial
+
+// Función para calcular el interés total:
+function calcularTotal(arr) {
+    const total = arr.reduce((acc, el)=>{
+        return acc + el.interesGanado
+    }, 0);
+    return total;
+}
+// fin de la función para calcular el interés total
 
 // Función para iniciar sesión:
 function login() {
@@ -19,7 +50,7 @@ function login() {
         } else {
             for (let i = 2; i >= 0; i--) {
                 let contraseniaIngresada = prompt("Ingresá tu contraseña:", "admin123");
-                if (contraseniaIngresada === null){
+                if (contraseniaIngresada === null) {
                     ingresar = null;
                     break;
                 } else if (contraseniaIngresada === contrasenia) {
@@ -39,8 +70,8 @@ function login() {
                             alert("Contraseña incorrecta. Te quedan " + i + " intentos.");
                         default:
                             break;
-                    }         
-                }                
+                    }
+                }
             }
         }
     } while (ingresar != true && ingresar != null && bloqueo != true);
@@ -76,18 +107,24 @@ function menu(capital, tasa1, tasa2, tasa3) {
 function plazoFijo(capital, tasa) {
     let aux = false;
     let plazo = prompt("PLAZO FIJO \n\nIngresá el plazo en días (mínimo 30 días - máximo 365 días):");
-    while (!aux && plazo != null) {   
+    while (!aux && plazo != null) {
         plazo = parseInt(plazo);
         if (plazo >= 30 && plazo <= 365) {
             let interesesGanados = (tasa / 100 / 365 * plazo) * capital;
             capital += interesesGanados;
             alert("INVERSIÓN EN PLAZO FIJO \n\nCapital final: $" + capital.toFixed(2) + "\nIntereses ganados: $" + interesesGanados.toFixed(2) + "\nPlazo: " + plazo + " día(s)\nTasa Nominal Anual: " + tasa + "%");
+            const nuevaInversion = new Inversion("Plazo Fijo", interesesGanados.toFixed(2), plazo, tasa, new Date().toLocaleString(), capital.toFixed(2));
+            cargarArray(historial, nuevaInversion);
+            console.log(historial);
+            let interesTotal = calcularTotal(historial);
+            alert("El interés total es: $" + interesTotal.toFixed(2));
+            console.log("El interés total es: " + interesTotal.toFixed(2));
             aux = true;
         } else {
             alert("El plazo no es válido.");
             plazo = prompt("PLAZO FIJO \n\nIngresá el plazo en días (mínimo 30 días - máximo 365 días):");
         }
-    } 
+    }
 }
 // fin de la función Plazo Fijo
 
@@ -95,18 +132,24 @@ function plazoFijo(capital, tasa) {
 function mercadopago(capital, tasa) {
     let aux = false;
     let plazo = prompt("MERCADOPAGO \n\nIngresá el plazo en días (mínimo 5 días - máximo 365 días):");
-    while (!aux && plazo != null) { 
+    while (!aux && plazo != null) {
         plazo = parseInt(plazo);
         if (plazo >= 5 && plazo <= 365) {
             let interesesGanados = (tasa / 100 / 365 * plazo) * capital;
             capital += interesesGanados;
             alert("INVERSIÓN EN MERCADOPAGO \n\nCapital final: $" + capital.toFixed(2) + "\nIntereses ganados: $" + interesesGanados.toFixed(2) + "\nPlazo: " + plazo + " día(s)\nTasa Nominal Anual: " + tasa + "%");
+            const nuevaInversion = new Inversion("Mercadopago", interesesGanados.toFixed(2), plazo, tasa, new Date().toLocaleString(), capital.toFixed(2));
+            cargarArray(historial, nuevaInversion);
+            console.log(historial);
+            let interesTotal = calcularTotal(historial);
+            alert("El interés total es: $" + interesTotal.toFixed(2));
+            console.log("El interés total es: " + interesTotal.toFixed(2));
             aux = true;
         } else {
             alert("El plazo no es válido.");
             plazo = prompt("MERCADOPAGO \n\nIngresá el plazo en días (mínimo 5 días - máximo 365 días):");
         }
-    }  
+    }
 }
 // fin de la función Mercadopago
 
@@ -114,18 +157,24 @@ function mercadopago(capital, tasa) {
 function caucion(capital, tasa) {
     let aux = false;
     let plazo = prompt("CAUCIÓN BURSÁTIL \n\nIngresá el plazo en días (mínimo 1 día - máximo 120 días):");
-    while (!aux && plazo != null) {  
+    while (!aux && plazo != null) {
         plazo = parseInt(plazo);
         if (plazo >= 1 && plazo <= 120) {
             let interesesGanados = (tasa / 100 / 365 * plazo) * capital;
             capital += interesesGanados;
             alert("INVERSIÓN EN CAUCIÓN BURSÁTIL \n\nCapital final: $" + capital.toFixed(2) + "\nIntereses ganados: $" + interesesGanados.toFixed(2) + "\nPlazo: " + plazo + " día(s)\nTasa Nominal Anual: " + tasa + "%");
+            const nuevaInversion = new Inversion("Caución", interesesGanados.toFixed(2), plazo, tasa, new Date().toLocaleString(), capital.toFixed(2));
+            cargarArray(historial, nuevaInversion);
+            console.log(historial);
+            let interesTotal = calcularTotal(historial);
+            alert("El interés total es: $" + interesTotal.toFixed(2));
+            console.log("El interés total es: " + interesTotal.toFixed(2));
             aux = true;
         } else {
             alert("El plazo no es válido.");
             plazo = prompt("CAUCIÓN BURSÁTIL \n\nIngresá el plazo en días (mínimo 1 día - máximo 120 días):");
         }
-    }   
+    }
 }
 //fin de la función Caución
 
@@ -133,7 +182,7 @@ function caucion(capital, tasa) {
 let ingreso = login();
 if (ingreso === null) {
     alert("Sesión cerrada.");
-} else if(ingreso) {
+} else if (ingreso) {
     let aux = false;
     let capitalAInvertir = prompt("Tu saldo actual es de $" + saldo + " \n\nIngresá el capital a invertir sin símbolos (monto mínimo $1.000.-):");
     console.log(capitalAInvertir);
@@ -143,13 +192,14 @@ if (ingreso === null) {
             alert("El campo no puede estar vacío ni contener símbolos.");
             capitalAInvertir = prompt("Tu saldo actual es de $" + saldo + " \n\nIngresá el capital a invertir sin símbolos (monto mínimo $1.000.-):");
         } else if (capitalAInvertir > saldo || capitalAInvertir < 1000) {
-            alert("Saldo insuficiente.");   
+            alert("Saldo insuficiente.");
             capitalAInvertir = prompt("Tu saldo actual es de $" + saldo + " \n\nIngresá el capital a invertir sin símbolos (monto mínimo $1.000.-):");
         } else {
             menu(capitalAInvertir, tasaPlazoFijo, tasaMercadopago, tasaCaucion);
             aux = true;
         }
-    }   
+    }
 } else {
     alert("Para desbloquear su cuenta por favor contacte al soporte.");
 }
+
