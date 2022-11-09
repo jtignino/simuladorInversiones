01078c340fdd072b99425bb9bcb84d5e5464a343
jtignino <jonathan.jat@hotmail.com>
2022-11-09 18:07:50 -0300

    Cambios en el HTML.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,27 @@ let contador = 0;
 const historial = [];
 
 // ---------- CONSTRUCTOR Y FUNCIONES ---------- //
+// Ejemplo de JavaScript inicial para deshabilitar el envío de formularios si hay campos no válidos
+// (function () {
+//     'use strict'
+  
+//     // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
+//     var forms = document.querySelectorAll('.needs-validation')
+  
+//     // Bucle sobre ellos y evitar el envío
+//     Array.prototype.slice.call(forms)
+//       .forEach(function (form) {
+//         form.addEventListener('submit', function (event) {
+//           if (!form.checkValidity()) {
+//             event.preventDefault()
+//             event.stopPropagation()
+//           }
+  
+//           form.classList.add('was-validated')
+//         }, false)
+//       })
+// })()
+
 // Creo la clase constructora de objetos que luego se guardarán en el historial:
 class Inversion {
     constructor(tipoInversion, interesGanado, plazo, tna, fecha, capitalFinal) {
